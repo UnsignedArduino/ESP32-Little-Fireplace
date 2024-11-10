f069e48a2bcb26d4e3ee1079cf679037769d0c07
Cyrus Yiu <UnsignedArduino@outlook.com>
2024-11-10 18:34:33 -0500

    Blink

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
 
 void setup() {
-  // write your initialization code here
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // write your code here
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
 }
