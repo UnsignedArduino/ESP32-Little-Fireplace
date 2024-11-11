a249b73ab642e6b596dd3dfaa263ae7b2c1415c1
Cyrus Yiu <UnsignedArduino@outlook.com>
2024-11-10 22:42:25 -0500

    Use framebuffer

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,7 @@ const uint8_t TFT_DC = 17;
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 AnimatedGIF gif;
 File file;
+uint16_t* framebuf = nullptr;
 
 void* GIFOpenFile(const char* fname, int32_t* pSize) {
   file = LittleFS.open(fname);
@@ -66,16 +67,24 @@ int32_t GIFSeekFile(GIFFILE* pFile, int32_t iPosition) {
 
 void GIFDraw(GIFDRAW* pDraw) {
   uint8_t* s;
-  uint16_t *d, *usPalette, usTemp[320];
-  int x, y, iWidth;
+  uint16_t *d, *usPalette;
+  int x, iWidth;
 
   iWidth = pDraw->iWidth;
   if (iWidth + pDraw->iX > tft.width())
     iWidth = tft.width() - pDraw->iX;
   usPalette = pDraw->pPalette;
-  y = pDraw->iY + pDraw->y; // current line
-  if (y >= tft.height() || pDraw->iX >= tft.width() || iWidth < 1)
+  if (pDraw->iY + pDraw->y >= tft.height() || pDraw->iX >= tft.width() ||
+      iWidth < 1)
     return;
+  if (pDraw->y == 0) { // start of frame, set address window on LCD
+    tft.dmaWait();     // wait for previous writes to complete before trying to
+                       // access the LCD
+    tft.setAddrWindow(pDraw->iX, pDraw->iY, pDraw->iWidth, pDraw->iHeight);
+    // By setting the address window to the size of the current GIF frame, we
+    // can just write continuously over the whole frame without having to set
+    // the address window again
+  }
   s = pDraw->pPixels;
   if (pDraw->ucDisposalMethod == 2) // restore to background color
   {
@@ -87,61 +96,28 @@ void GIFDraw(GIFDRAW* pDraw) {
   }
 
   // Apply the new pixels to the main image
+  d = &framebuf[pDraw->iWidth * pDraw->y];
   if (pDraw->ucHasTransparency) // if transparency used
   {
-    uint8_t *pEnd, c, ucTransparent = pDraw->ucTransparent;
-    int x, iCount;
-    pEnd = s + iWidth;
-    x = 0;
-    iCount = 0; // count non-transparent pixels
-    while (x < iWidth) {
-      c = ucTransparent - 1;
-      d = usTemp;
-      while (c != ucTransparent && s < pEnd) {
-        c = *s++;
-        if (c == ucTransparent) // done, stop
-        {
-          s--; // back up to treat it like transparent
-        } else // opaque
-        {
-          *d++ = usPalette[c];
-          iCount++;
-        }
-      } // while looking for opaque pixels
-      if (iCount) // any opaque pixels?
-      {
-        tft.startWrite();
-        tft.setAddrWindow(pDraw->iX + x, y, iCount, 1);
-        tft.writePixels(usTemp, iCount, false, false);
-        tft.endWrite();
-        x += iCount;
-        iCount = 0;
-      }
-      // no, look for a run of transparent pixels
-      c = ucTransparent;
-      while (c == ucTransparent && s < pEnd) {
-        c = *s++;
-        if (c == ucTransparent)
-          iCount++;
-        else
-          s--;
-      }
-      if (iCount) {
-        x += iCount; // skip these
-        iCount = 0;
-      }
+    uint8_t c, ucTransparent = pDraw->ucTransparent;
+    int x;
+    for (x = 0; x < iWidth; x++) {
+      c = *s++;
+      if (c != ucTransparent)
+        d[x] = usPalette[c];
     }
   } else {
-    s = pDraw->pPixels;
     // Translate the 8-bit pixels through the RGB565 palette (already byte
     // reversed)
-    for (x = 0; x < iWidth; x++)
-      usTemp[x] = usPalette[*s++];
-    tft.startWrite();
-    tft.setAddrWindow(pDraw->iX, y, iWidth, 1);
-    tft.writePixels(usTemp, iWidth, false, false);
-    tft.endWrite();
+    for (x = 0; x < iWidth; x++) {
+      d[x] = usPalette[s[x]];
+    }
   }
+  tft.dmaWait(); // wait for last write to complete (the last scan line)
+  // We write with block set to FALSE (3rd param) so that we can be decoding the
+  // next line while the DMA hardware continues to write data to the LCD
+  // controller
+  tft.writePixels(d, iWidth, false, false);
 }
 
 void setup() {
@@ -165,6 +141,16 @@ void setup() {
     HALT();
   }
 
+  const uint16_t fbSize = tft.width() * tft.height() * sizeof(uint16_t);
+  framebuf = (uint16_t*)malloc(fbSize);
+  if (framebuf == nullptr) {
+    logger.printf("Failed to allocate framebuffer of size %d bytes\n", fbSize);
+    HALT();
+  } else {
+    memset(framebuf, 0, tft.width() * tft.height() * sizeof(uint16_t));
+    logger.printf("Allocated framebuffer of size %d bytes\n", fbSize);
+  }
+
   gif.begin(LITTLE_ENDIAN_PIXELS);
 
   logger.println("Opening GIF file /fireplace.gif");
@@ -178,6 +164,9 @@ void setup() {
       logger.printf("duration: %d ms\n", gi.iDuration);
       logger.printf("max delay: %d ms\n", gi.iMaxDelay);
       logger.printf("min delay: %d ms\n", gi.iMinDelay);
+    } else {
+      logger.println("Failed to get GIF info");
+      HALT();
     }
 #ifdef DEBUG
     delay(1000);
@@ -187,7 +176,9 @@ void setup() {
       uint32_t frameStart = millis();
       while (true) {
         int toDelay;
+        tft.startWrite();
         int result = gif.playFrame(false, &toDelay);
+        tft.endWrite();
         uint32_t frameDecodeEnd = millis();
         if (result == 0) {
           if (gif.getLastError() != GIF_SUCCESS &&
