1f99e76867584c372c1dbfd8ec88ea018a3d5249
Cyrus Yiu <UnsignedArduino@outlook.com>
2024-11-10 19:14:35 -0500

    Working with TFT

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,21 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h>
 #include <Arduino.h>
+#include <SPI.h>
+
+const uint8_t TFT_CS = 5;
+const uint8_t TFT_RST = 16;
+const uint8_t TFT_DC = 17;
+
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+
+  tft.initR(INITR_BLACKTAB);
+  tft.setSPISpeed(40000000);
+
+  tft.fillScreen(ST77XX_BLACK);
 }
 
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
-}
+void loop() {}
